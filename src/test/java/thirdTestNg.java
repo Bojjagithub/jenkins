28cfd8019f50d5840f80923eda3ac54c28e1592d
Vinod <vinodreddy.b@gmail.com>
2020-08-31 10:34:11 +0530

    initial Commits

--- src/test/java/thirdTestNg.java
+++ src/test/java/thirdTestNg.java
@@ -6,13 +6,9 @@ public class thirdTestNg {
     public void mytest1(){
     System.out.println(" hello Reddy from third TestNg");
 
-
 }
-
     @Test
     public void mytest2(){
         System.out.println(" hello Reddy2 from third TestNg");
-
-
     }
 }
