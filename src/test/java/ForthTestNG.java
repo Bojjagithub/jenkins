4e5f2e2c95bf2b0806a8b6046a0b74fa7cfae790
Vinod <vinodreddy.b@gmail.com>
2020-08-31 10:42:43 +0530

    initial Commits

---
+++ src/test/java/ForthTestNG.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class ForthTestNG {
+
+
+    @Test
+    public void mytest1(){
+        System.out.println(" hello Reddy from Forth TestNg");
+        System.out.println(" hello Reddy from Forth TestNg");
+
+    }
+}
